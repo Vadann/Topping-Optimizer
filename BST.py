f88358d5d083b38894fa6bbc45c84ac93d2edc69
Hayk <haykvadan@gmail.com>
2024-10-26 21:52:02 -0700

    BST

---
+++ BST.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def add(self, current, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            if val < current.val:
+                if not current.left:
+                    current.left = Node(val)
+                else:
+                    self.add(current.left, val)
+
+            elif val > current.val:
+                if not current.right:
+                    current.right = Node(val)
+                else:
+                    self.add(current.right, val)
+
+    def preorder(self, root):
+        if root is None:
+            return
+        print(root.val)
+        self.preorder(root.left)
+        self.preorder(root.right)
+
+
